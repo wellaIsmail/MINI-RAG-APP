a294046e126720696724f4de89c8970ac8291782
Fakhreldin Ismail <waleed.iwave@gmail.com>
2025-06-03 02:15:28 +0300

    tut-006

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -8,6 +8,9 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES:list
     FILE_MAX_SIZE:int
     FILE_DEFAULT_CHUNK_SIZE:int
+
+    MONGODB_URL:str
+    MONGODB_DATABASE:str
     class Config:
         env_file=".env"
 
