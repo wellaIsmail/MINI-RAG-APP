edf3b614ebe38ff20e635719056dd32eeddc2c7d
Fakhreldin Ismail <waleed.iwave@gmail.com>
2025-06-06 04:37:52 +0300

    final tut

---
+++ src/models/db_schemes/asset.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, Field, validator
+from typing import Optional
+from bson.objectid import ObjectId
+from datetime import datetime
+
+class Asset(BaseModel):
+    id:Optional[ObjectId] = Field(None, alias = "_id")
+    asset_project_id:ObjectId
+    asset_type:str = Field(..., min_length=1)
+    asset_name:str = Field(..., min_length=1)
+    asset_size:int = Field(ge=0, default=None)
+    asset_config:dict = Field(default=None)
+    asset_pushed_at:datetime = Field(default= datetime.utcnow)
+
+
+
+    model_config = {
+        "arbitrary_types_allowed": True,
+        "populate_by_name": True
+    }
+
+    @classmethod
+    def get_indexes(cls):
+
+        return [
+            {
+                "key": [
+                    ("asset_project_id", 1)
+                ],
+                "name": "asset_project_id_index_1",
+                "unique": False
+            },
+            {
+                "key": [
+                    ("asset_project_id", 1),
+                    ("asset_name", 1)
+                ],
+                "name": "asset_project_id_name_index_1",
+                "unique": True
+            }
+        ]
